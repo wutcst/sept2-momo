f0d286047e719dcedf7fcc0621ebab61261ef2cd
GrayFluex <1055210335@qq.com>
2023-06-24 17:46:13 +0800

    修改随机获得物品的逻辑

--- game-server/src/main/java/cn/edu/whut/sept/zuul/command/GetCommand.java
+++ game-server/src/main/java/cn/edu/whut/sept/zuul/command/GetCommand.java
@@ -2,8 +2,9 @@ package cn.edu.whut.sept.zuul.command;
 
 import cn.edu.whut.sept.zuul.Game;
 
+import java.util.Random;
+
 public class GetCommand {
-    @Override
     public void execute() {
         System.out.println("执行 get 命令");
 
@@ -22,11 +23,11 @@ public class GetCommand {
     private String getRandomItem() {
         // 模拟随机获取物品的逻辑
         String[] items = {"钥匙", "宝剑", "药水", "金币"};
-        int randomIndex = (int) (Math.random() * items.length);
+        int randomIndex = new Random().nextInt(items.length);
         return items[randomIndex];
     }
-
 }
+
 
 
 
