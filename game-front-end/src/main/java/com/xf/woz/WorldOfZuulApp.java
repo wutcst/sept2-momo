66d97eb6af6de2d56c2ee554fc749f27372174e6
GrayFluex <1055210335@qq.com>
2023-06-24 22:09:51 +0800

    feature/主界面逻辑修改

--- game-front-end/src/main/java/com/xf/woz/WorldOfZuulApp.java
+++ game-front-end/src/main/java/com/xf/woz/WorldOfZuulApp.java
@@ -128,8 +128,237 @@ public class WorldOfZuulApp extends GameApplication {
 
     @Override
     protected void initUI() {
-        // 设置 UI 样式
-        FXGLUtils.loadCSS("style.css");
+        String[] ui = {"help", "go", "quit", "look", "back", "take", "drop", "items"};
+        Image image = new Image("assets/textures/ui/button.png");
+
+        Rectangle r1 = new Rectangle(180, 64);
+        r1.setFill(new ImagePattern(image));
+        Text t1 = FXGL.getUIFactoryService().newText(ui[0]);
+        StackPane help = new StackPane(r1, t1);
+        List<String> helpTalks = List.of("You are lost. You are alone.",
+                "You wander around at the university.",
+                "Your command words are left.");
+        List<Talk> talks1 = TalkFactory.buildTalkList(helpTalks);
+        help.setOnMouseClicked(mouseEvent -> Platform.runLater(() -> TalkScene.getInstance().show(talks1)));
+        addUINode(help, 0, 30);
+
+
+        Rectangle r2 = new Rectangle(180, 64);
+        r2.setFill(new ImagePattern(image));
+        Text t2 = FXGL.getUIFactoryService().newText(ui[1]);
+        StackPane go = new StackPane(r2, t2);
+
+        go.setOnMouseClicked(mouseEvent -> Platform.runLater(() -> {
+            List<String> place = FXGLUtils.nowPlayer.getPlace();
+            nowPlace = place.subList(place.size() - 1, place.size()).get(0);
+
+            List<String> exits = geto(nowPlace);
+            List<String> goTalks = List.of("Go where?",
+                    "Exits:" + exits);
+            List<Talk> talks2 = TalkFactory.buildTalkList(goTalks);
+            TalkScene.getInstance().show(talks2);
+
+            List<StackPane> dirNodes = new ArrayList<>();
+
+            for (int i = 0; i < exits.size(); i++) {
+                Rectangle rect = new Rectangle(180, 64);
+                rect.setFill(new ImagePattern(image));
+                Text text = FXGL.getUIFactoryService().newText(exits.get(i));
+                StackPane stackPane = new StackPane(rect, text);
+                int finalI = i;
+                stackPane.setOnMouseClicked(mouseEventDir -> {
+                    dirNodes.forEach(FXGL::removeUINode);
+                    try {
+                        String s = FXGLUtils.newRoom(nowPlace, exits.get(finalI));
+                        FXGLUtils.updateRoom(s);
+                    } catch (NoSuchFieldException | IllegalAccessException e) {
+                        e.printStackTrace();
+                    }
+                });
+
+                dirNodes.add(stackPane);
+                addUINode(stackPane, 200, 80 * (i + 1) + 30);
+            }
+
+        }));
+        addUINode(go, 0, 80 + 30);
+
+        Rectangle r3 = new Rectangle(180, 64);
+        r3.setFill(new ImagePattern(image));
+        Text t3 = FXGL.getUIFactoryService().newText(ui[2]);
+        StackPane quit = new StackPane(r3, t3);
+        quit.setOnMouseClicked(mouseEventQuit -> Platform.runLater(() -> {
+            List<String> quitStrings = List.of("Thank you for playing.  Good bye.",
+                    "We will save the data for you.");
+            List<Talk> talks3 = TalkFactory.buildTalkList(quitStrings);
+            TalkScene.getInstance().show(talks3);
+            FXGLUtils.updateAllRoom();
+            FXGLUtils.updatePlayer();
+        }));
+        addUINode(quit, 0, 80 * 2 + 30);
+
+        Rectangle r4 = new Rectangle(180, 64);
+        r4.setFill(new ImagePattern(image));
+        Text t4 = FXGL.getUIFactoryService().newText(ui[3]);
+        StackPane look = new StackPane(r4, t4);
+        look.setOnMouseClicked(mouseEvent -> {
+            List<String> place = FXGLUtils.nowPlayer.getPlace();
+            nowPlace = place.subList(place.size() - 1, place.size()).get(0);
+            List<String> lookTalks = List.of("You are in the " + nowPlace + ".",
+                    "You can see:" + FXGLUtils.roomItems.get(nowPlace));
+            List<Talk> talks4 = TalkFactory.buildTalkList(lookTalks);
+            TalkScene.getInstance().show(talks4);
+        });
+        addUINode(look, 0, 80 * 3 + 30);
+
+        Rectangle r5 = new Rectangle(180, 64);
+        r5.setFill(new ImagePattern(image));
+        Text t5 = FXGL.getUIFactoryService().newText(ui[4]);
+        StackPane back = new StackPane(r5, t5);
+        back.setOnMouseClicked(mouseEventBack -> {
+            FXGLUtils.backRoom();
+        });
+        addUINode(back, 0, 80 * 4 + 30);
+
+        Rectangle r6 = new Rectangle(180, 64);
+        r6.setFill(new ImagePattern(image));
+        Text t6 = FXGL.getUIFactoryService().newText(ui[5]);
+        StackPane take = new StackPane(r6, t6);
+        take.setOnMouseClicked(mouseEvent -> {
+            List<String> place = FXGLUtils.nowPlayer.getPlace();
+            nowPlace = place.subList(place.size() - 1, place.size()).get(0);
+            List<String> takeTalks = List.of("Take what?",
+                    "Items:" + FXGLUtils.roomItems.get(nowPlace));
+            List<Talk> talks6 = TalkFactory.buildTalkList(takeTalks);
+            TalkScene.getInstance().show(talks6);
+
+            List<StackPane> itemNodes = new ArrayList<>();
+
+            Map<String, Integer> itemCount = FXGLUtils.roomItems.get(nowPlace);
+            ArrayList<String> itemsName = new ArrayList<>(itemCount.keySet());
+            int uiPlace = 5;
+
+            for (int i = 0; i < FXGLUtils.roomItems.get(nowPlace).size(); i++) {
+                String s = itemsName.get(i);
+                if (itemCount.get(s) == 0) {
+                    continue;
+                }
+                Rectangle rect = new Rectangle(180, 64);
+                rect.setFill(new ImagePattern(image));
+                Text text = FXGL.getUIFactoryService().newText(s);
+                StackPane stackPane = new StackPane(rect, text);
+                stackPane.setOnMouseClicked(mouseEventDir -> {
+                    itemNodes.forEach(FXGL::removeUINode);
+                    try {
+                        FXGLUtils.takeItem(nowPlace, s);
+                        FXGL.getNotificationService().pushNotification("You take " + s);
+                    } catch (NoSuchFieldException | IllegalAccessException e) {
+                        e.printStackTrace();
+                    }
+                });
+
+                itemNodes.add(stackPane);
+                addUINode(stackPane, 200, 80 * uiPlace + 30);
+                uiPlace++;
+            }
+        });
+        addUINode(take, 0, 80 * 5 + 30);
+
+        Rectangle r7 = new Rectangle(180, 64);
+        r7.setFill(new ImagePattern(image));
+        Text t7 = FXGL.getUIFactoryService().newText(ui[6]);
+        StackPane drop = new StackPane(r7, t7);
+        drop.setOnMouseClicked(mouseEvent -> {
+            List<String> place = FXGLUtils.nowPlayer.getPlace();
+            nowPlace = place.subList(place.size() - 1, place.size()).get(0);
+            List<String> dropTalks = List.of("Drop what?",
+                    "Items:" + FXGLUtils.nowPlayer.getItems());
+            List<Talk> talks7 = TalkFactory.buildTalkList(dropTalks);
+            TalkScene.getInstance().show(talks7);
+
+            List<StackPane> itemNodes = new ArrayList<>();
+
+            List<String> itemList = FXGLUtils.nowPlayer.getItems();
+            //统计itemList中各元素出现次数
+            Map<String, Integer> itemCount = new HashMap<>();
+            for (String item : itemList) {
+                Integer count = itemCount.get(item);
+                itemCount.put(item, (count == null) ? 1 : count + 1);
+            }
+            ArrayList<String> itemsName = new ArrayList<>(itemCount.keySet());
+            int uiPlace = 6;
+
+            if (itemsName.size() != 0) {
+                Rectangle rect = new Rectangle(180, 64);
+                rect.setFill(new ImagePattern(image));
+                Text text = FXGL.getUIFactoryService().newText("all");
+                StackPane stackPane = new StackPane(rect, text);
+                stackPane.setOnMouseClicked(mouseEventDir -> {
+                    itemNodes.forEach(FXGL::removeUINode);
+                    try {
+                        FXGLUtils.dropAllItem(nowPlace);
+                        FXGL.getNotificationService().pushNotification("You drop all");
+                    } catch (NoSuchFieldException | IllegalAccessException e) {
+                        e.printStackTrace();
+                    }
+                });
+                itemNodes.add(stackPane);
+                addUINode(stackPane, 200, 80 * uiPlace + 30);
+                uiPlace++;
+            }
+            for (int i = 0; i < itemCount.size(); i++) {
+                String s = itemsName.get(i);
+                if (itemCount.get(s) == 0) {
+                    continue;
+                }
+                Rectangle r = new Rectangle(180, 64);
+                r.setFill(new ImagePattern(image));
+                Text t = FXGL.getUIFactoryService().newText(s);
+                StackPane stackPane1 = new StackPane(r, t);
+                stackPane1.setOnMouseClicked(mouseEventDir -> {
+                    itemNodes.forEach(FXGL::removeUINode);
+                    try {
+                        FXGLUtils.dropItem(nowPlace, s);
+                        FXGL.getNotificationService().pushNotification("You drop " + s);
+                    } catch (NoSuchFieldException | IllegalAccessException e) {
+                        e.printStackTrace();
+                    }
+                });
+
+                itemNodes.add(stackPane1);
+                addUINode(stackPane1, 200, 80 * uiPlace + 30);
+                uiPlace++;
+            }
+
+        });
+        addUINode(drop, 0, 80 * 6 + 30);
+
+        Rectangle r8 = new Rectangle(180, 64);
+        r8.setFill(new ImagePattern(image));
+        Text t8 = FXGL.getUIFactoryService().newText(ui[7]);
+        StackPane items = new StackPane(r8, t8);
+        items.setOnMouseClicked(mouseEvent -> {
+            List<String> place = FXGLUtils.nowPlayer.getPlace();
+            nowPlace = place.subList(place.size() - 1, place.size()).get(0);
+            List<String> itemsTalks = new ArrayList<>(List.of("You have:" + FXGLUtils.nowPlayer.getItems(),
+                    "You can see:" + FXGLUtils.roomItems.get(nowPlace)));
+            if (FXGLUtils.nowPlayer.getItems().contains("magic cookie")) {
+                itemsTalks.add("You can use magic cookie to increase load.");
+
+                Rectangle rect = new Rectangle(180, 64);
+                rect.setFill(new ImagePattern(image));
+                Text text = FXGL.getUIFactoryService().newText("use magic cookie");
+                StackPane stackPane = new StackPane(rect, text);
+                stackPane.setOnMouseClicked(mouseEventDir -> {
+                    removeUINode(stackPane);
+                    FXGLUtils.useMagicCookie();
+                });
+                addUINode(stackPane, 200, 80 * 7 + 30);
+            }
+            List<Talk> talks5 = TalkFactory.buildTalkList(itemsTalks);
+            TalkScene.getInstance().show(talks5);
+        });
+        addUINode(items, 0, 80 * 7 + 30);
     }
 
     @Override
