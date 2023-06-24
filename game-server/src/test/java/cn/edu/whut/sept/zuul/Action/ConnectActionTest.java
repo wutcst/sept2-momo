bbabbc0b89cb9653854509c29df49703719e897d
fshoes <105585532+fshoes@users.noreply.github.com>
2023-06-24 22:00:17 +0800

    代码规范化

--- game-server/src/test/java/cn/edu/whut/sept/zuul/Action/ConnectActionTest.java
+++ game-server/src/test/java/cn/edu/whut/sept/zuul/Action/ConnectActionTest.java
@@ -25,7 +25,228 @@ public class ConnectActionTest {
         RoomProtoBuf roomProtoBuf1 = connectAction.updateAllRoom(roomProtoBuf, null);
         assert roomProtoBuf1.getItems().get(0).equals("magic cookie");
     }
+ */
+package cn.edu.whut.sept.zuul.Action;
 
+import cn.edu.whut.sept.zuul.action.ConnectAction;
+import cn.edu.whut.sept.zuul.protoBuf.RoomProtoBuf;
+import org.junit.Test;
+
+import java.util.List;
+
+public class ConnectActionTest {
+    @Test
+    public void updateAllRoomTest(){
+        ConnectAction connectAction = new ConnectAction();
+        RoomProtoBuf roomProtoBuf = new RoomProtoBuf();
+        roomProtoBuf.setName("pub");
+        roomProtoBuf.setItems(List.of("magic cookie","apple","apple","apple","apple","apple","apple","apple","apple","apple", "alcoholic chocolate"));
+        RoomProtoBuf roomProtoBuf1 = connectAction.updateAllRoom(roomProtoBuf, null);
+        assert roomProtoBuf1.getItems().get(0).equals("magic cookie"); */
+package cn.edu.whut.sept.zuul.Action;
+
+import cn.edu.whut.sept.zuul.action.ConnectAction;
+import cn.edu.whut.sept.zuul.protoBuf.RoomProtoBuf;
+import org.junit.Test;
+
+import java.util.List;
+
+public class ConnectActionTest {
+    @Test
+    public void updateAllRoomTest(){
+        ConnectAction connectAction = new ConnectAction();
+        RoomProtoBuf roomProtoBuf = new RoomProtoBuf();
+        roomProtoBuf.setName("pub");
+        roomProtoBuf.setItems(List.of("magic cookie","apple","apple","apple","apple","apple","apple","apple","apple","apple", "alcoholic chocolate"));
+        RoomProtoBuf roomProtoBuf1 = connectAction.updateAllRoom(roomProtoBuf, null);
+        assert roomProtoBuf1.getItems().get(0).equals("magic cookie"); */
+package cn.edu.whut.sept.zuul.Action;
+
+import cn.edu.whut.sept.zuul.action.ConnectAction;
+import cn.edu.whut.sept.zuul.protoBuf.RoomProtoBuf;
+import org.junit.Test;
+
+import java.util.List;
+
+public class ConnectActionTest {
+    @Test
+    public void updateAllRoomTest(){
+        ConnectAction connectAction = new ConnectAction();
+        RoomProtoBuf roomProtoBuf = new RoomProtoBuf();
+        roomProtoBuf.setName("pub");
+        roomProtoBuf.setItems(List.of("magic cookie","apple","apple","apple","apple","apple","apple","apple","apple","apple", "alcoholic chocolate"));
+        RoomProtoBuf roomProtoBuf1 = connectAction.updateAllRoom(roomProtoBuf, null);
+        assert roomProtoBuf1.getItems().get(0).equals("magic cookie"); */
+package cn.edu.whut.sept.zuul.Action;
+
+import cn.edu.whut.sept.zuul.action.ConnectAction;
+import cn.edu.whut.sept.zuul.protoBuf.RoomProtoBuf;
+import org.junit.Test;
+
+import java.util.List;
+
+public class ConnectActionTest {
+    @Test
+    public void updateAllRoomTest(){
+        ConnectAction connectAction = new ConnectAction();
+        RoomProtoBuf roomProtoBuf = new RoomProtoBuf();
+        roomProtoBuf.setName("pub");
+        roomProtoBuf.setItems(List.of("magic cookie","apple","apple","apple","apple","apple","apple","apple","apple","apple", "alcoholic chocolate"));
+        RoomProtoBuf roomProtoBuf1 = connectAction.updateAllRoom(roomProtoBuf, null);
+        assert roomProtoBuf1.getItems().get(0).equals("magic cookie"); */
+package cn.edu.whut.sept.zuul.Action;
+
+import cn.edu.whut.sept.zuul.action.ConnectAction;
+import cn.edu.whut.sept.zuul.protoBuf.RoomProtoBuf;
+import org.junit.Test;
+
+import java.util.List;
+
+public class ConnectActionTest {
+    @Test
+    public void updateAllRoomTest(){
+        ConnectAction connectAction = new ConnectAction();
+        RoomProtoBuf roomProtoBuf = new RoomProtoBuf();
+        roomProtoBuf.setName("pub");
+        roomProtoBuf.setItems(List.of("magic cookie","apple","apple","apple","apple","apple","apple","apple","apple","apple", "alcoholic chocolate"));
+        RoomProtoBuf roomProtoBuf1 = connectAction.updateAllRoom(roomProtoBuf, null);
+        assert roomProtoBuf1.getItems().get(0).equals("magic cookie"); */
+package cn.edu.whut.sept.zuul.Action;
+
+import cn.edu.whut.sept.zuul.action.ConnectAction;
+import cn.edu.whut.sept.zuul.protoBuf.RoomProtoBuf;
+import org.junit.Test;
+
+import java.util.List;
+
+public class ConnectActionTest {
+    @Test
+    public void updateAllRoomTest(){
+        ConnectAction connectAction = new ConnectAction();
+        RoomProtoBuf roomProtoBuf = new RoomProtoBuf();
+        roomProtoBuf.setName("pub");
+        roomProtoBuf.setItems(List.of("magic cookie","apple","apple","apple","apple","apple","apple","apple","apple","apple", "alcoholic chocolate"));
+        RoomProtoBuf roomProtoBuf1 = connectAction.updateAllRoom(roomProtoBuf, null);
+        assert roomProtoBuf1.getItems().get(0).equals("magic cookie"); */
+package cn.edu.whut.sept.zuul.Action;
+
+import cn.edu.whut.sept.zuul.action.ConnectAction;
+import cn.edu.whut.sept.zuul.protoBuf.RoomProtoBuf;
+import org.junit.Test;
+
+import java.util.List;
+
+public class ConnectActionTest {
+    @Test
+    public void updateAllRoomTest(){
+        ConnectAction connectAction = new ConnectAction();
+        RoomProtoBuf roomProtoBuf = new RoomProtoBuf();
+        roomProtoBuf.setName("pub");
+        roomProtoBuf.setItems(List.of("magic cookie","apple","apple","apple","apple","apple","apple","apple","apple","apple", "alcoholic chocolate"));
+        RoomProtoBuf roomProtoBuf1 = connectAction.updateAllRoom(roomProtoBuf, null);
+        assert roomProtoBuf1.getItems().get(0).equals("magic cookie"); */
+package cn.edu.whut.sept.zuul.Action;
+
+import cn.edu.whut.sept.zuul.action.ConnectAction;
+import cn.edu.whut.sept.zuul.protoBuf.RoomProtoBuf;
+import org.junit.Test;
+
+import java.util.List;
+
+public class ConnectActionTest {
+    @Test
+    public void updateAllRoomTest(){
+        ConnectAction connectAction = new ConnectAction();
+        RoomProtoBuf roomProtoBuf = new RoomProtoBuf();
+        roomProtoBuf.setName("pub");
+        roomProtoBuf.setItems(List.of("magic cookie","apple","apple","apple","apple","apple","apple","apple","apple","apple", "alcoholic chocolate"));
+        RoomProtoBuf roomProtoBuf1 = connectAction.updateAllRoom(roomProtoBuf, null);
+        assert roomProtoBuf1.getItems().get(0).equals("magic cookie"); */
+package cn.edu.whut.sept.zuul.Action;
+
+import cn.edu.whut.sept.zuul.action.ConnectAction;
+import cn.edu.whut.sept.zuul.protoBuf.RoomProtoBuf;
+import org.junit.Test;
+
+import java.util.List;
+
+public class ConnectActionTest {
+    @Test
+    public void updateAllRoomTest(){
+        ConnectAction connectAction = new ConnectAction();
+        RoomProtoBuf roomProtoBuf = new RoomProtoBuf();
+        roomProtoBuf.setName("pub");
+        roomProtoBuf.setItems(List.of("magic cookie","apple","apple","apple","apple","apple","apple","apple","apple","apple", "alcoholic chocolate"));
+        RoomProtoBuf roomProtoBuf1 = connectAction.updateAllRoom(roomProtoBuf, null);
+        assert roomProtoBuf1.getItems().get(0).equals("magic cookie"); */
+package cn.edu.whut.sept.zuul.Action;
+
+import cn.edu.whut.sept.zuul.action.ConnectAction;
+import cn.edu.whut.sept.zuul.protoBuf.RoomProtoBuf;
+import org.junit.Test;
+
+import java.util.List;
+
+public class ConnectActionTest {
+    @Test
+    public void updateAllRoomTest(){
+        ConnectAction connectAction = new ConnectAction();
+        RoomProtoBuf roomProtoBuf = new RoomProtoBuf();
+        roomProtoBuf.setName("pub");
+        roomProtoBuf.setItems(List.of("magic cookie","apple","apple","apple","apple","apple","apple","apple","apple","apple", "alcoholic chocolate"));
+        RoomProtoBuf roomProtoBuf1 = connectAction.updateAllRoom(roomProtoBuf, null);
+        assert roomProtoBuf1.getItems().get(0).equals("magic cookie"); */
+package cn.edu.whut.sept.zuul.Action;
+
+import cn.edu.whut.sept.zuul.action.ConnectAction;
+import cn.edu.whut.sept.zuul.protoBuf.RoomProtoBuf;
+import org.junit.Test;
+
+import java.util.List;
+
+public class ConnectActionTest {
+    @Test
+    public void updateAllRoomTest(){
+        ConnectAction connectAction = new ConnectAction();
+        RoomProtoBuf roomProtoBuf = new RoomProtoBuf();
+        roomProtoBuf.setName("pub");
+        roomProtoBuf.setItems(List.of("magic cookie","apple","apple","apple","apple","apple","apple","apple","apple","apple", "alcoholic chocolate"));
+        RoomProtoBuf roomProtoBuf1 = connectAction.updateAllRoom(roomProtoBuf, null);
+        assert roomProtoBuf1.getItems().get(0).equals("magic cookie"); */
+package cn.edu.whut.sept.zuul.Action;
+
+import cn.edu.whut.sept.zuul.action.ConnectAction;
+import cn.edu.whut.sept.zuul.protoBuf.RoomProtoBuf;
+import org.junit.Test;
+
+import java.util.List;
+
+public class ConnectActionTest {
+    @Test
+    public void updateAllRoomTest(){
+        ConnectAction connectAction = new ConnectAction();
+        RoomProtoBuf roomProtoBuf = new RoomProtoBuf();
+        roomProtoBuf.setName("pub");
+        roomProtoBuf.setItems(List.of("magic cookie","apple","apple","apple","apple","apple","apple","apple","apple","apple", "alcoholic chocolate"));
+        RoomProtoBuf roomProtoBuf1 = connectAction.updateAllRoom(roomProtoBuf, null);
+        assert roomProtoBuf1.getItems().get(0).equals("magic cookie"); */
+package cn.edu.whut.sept.zuul.Action;
+
+import cn.edu.whut.sept.zuul.action.ConnectAction;
+import cn.edu.whut.sept.zuul.protoBuf.RoomProtoBuf;
+import org.junit.Test;
+
+import java.util.List;
+
+public class ConnectActionTest {
+    @Test
+    public void updateAllRoomTest(){
+        ConnectAction connectAction = new ConnectAction();
+        RoomProtoBuf roomProtoBuf = new RoomProtoBuf();
+        roomProtoBuf.setName("pub");
+        roomProtoBuf.setItems(List.of("magic cookie","apple","apple","apple","apple","apple","apple","apple","apple","apple", "alcoholic chocolate"));
+        RoomProtoBuf roomProtoBuf1 = connectAction.updateAllRoom(roomProtoBuf, null);
+        assert roomProtoBuf1.getItems().get(0).equals("magic cookie");
 
     }
 
