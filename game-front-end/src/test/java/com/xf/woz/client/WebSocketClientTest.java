b2250db235aec0a05109b27f79628d48ce5eebbc
fshoes <105585532+fshoes@users.noreply.github.com>
2023-06-25 00:11:41 +0800

    Update WebSocketClientTest.java

--- game-front-end/src/test/java/com/xf/woz/client/WebSocketClientTest.java
+++ game-front-end/src/test/java/com/xf/woz/client/WebSocketClientTest.java
@@ -32,3 +32,159 @@ public class WebSocketClientTest {
         // 游戏框架内置的协议， 与游戏前端相互通信的协议
     }
 }
+ */
+package com.xf.woz.client;
+
+import cn.hutool.json.JSONUtil;
+import com.iohao.game.bolt.broker.client.external.bootstrap.ExternalKit;
+import com.iohao.game.bolt.broker.client.external.bootstrap.message.ExternalMessage;
+import com.xf.woz.protoBuf.ConnectVerify;
+import org.junit.Test;
+
+public class WebSocketClientTest {
+    @Test
+    public void decodeTest(){
+        int cmd = 0;
+        int subCmd = 0;
+        ConnectVerify connectReq = new ConnectVerify();
+
+        connectReq.setMsg("jack");
+        String s = JSONUtil.toJsonStr(connectReq);
+        ExternalMessage externalMessage = ExternalKit
+                // 路由、子路由、业务数据
+                .createExternalMessage(cmd, subCmd, connectReq);
+        System.out.println(externalMessage);
+//        System.out.println("CommentWebSocketClient.onOpen");
+        // 游戏框架内置的协议， 与游戏前端相互通信的协议
+    }
+}
+ */
+package com.xf.woz.client;
+
+import cn.hutool.json.JSONUtil;
+import com.iohao.game.bolt.broker.client.external.bootstrap.ExternalKit;
+import com.iohao.game.bolt.broker.client.external.bootstrap.message.ExternalMessage;
+import com.xf.woz.protoBuf.ConnectVerify;
+import org.junit.Test;
+
+public class WebSocketClientTest {
+    @Test
+    public void decodeTest(){
+        int cmd = 0;
+        int subCmd = 0;
+        ConnectVerify connectReq = new ConnectVerify();
+
+        connectReq.setMsg("jack");
+        String s = JSONUtil.toJsonStr(connectReq);
+        ExternalMessage externalMessage = ExternalKit
+                // 路由、子路由、业务数据
+                .createExternalMessage(cmd, subCmd, connectReq);
+        System.out.println(externalMessage);
+//        System.out.println("CommentWebSocketClient.onOpen");
+        // 游戏框架内置的协议， 与游戏前端相互通信的协议
+    }
+}
+ */
+package com.xf.woz.client;
+
+import cn.hutool.json.JSONUtil;
+import com.iohao.game.bolt.broker.client.external.bootstrap.ExternalKit;
+import com.iohao.game.bolt.broker.client.external.bootstrap.message.ExternalMessage;
+import com.xf.woz.protoBuf.ConnectVerify;
+import org.junit.Test;
+
+public class WebSocketClientTest {
+    @Test
+    public void decodeTest(){
+        int cmd = 0;
+        int subCmd = 0;
+        ConnectVerify connectReq = new ConnectVerify();
+
+        connectReq.setMsg("jack");
+        String s = JSONUtil.toJsonStr(connectReq);
+        ExternalMessage externalMessage = ExternalKit
+                // 路由、子路由、业务数据
+                .createExternalMessage(cmd, subCmd, connectReq);
+        System.out.println(externalMessage);
+//        System.out.println("CommentWebSocketClient.onOpen");
+        // 游戏框架内置的协议， 与游戏前端相互通信的协议
+    }
+}
+ */
+package com.xf.woz.client;
+
+import cn.hutool.json.JSONUtil;
+import com.iohao.game.bolt.broker.client.external.bootstrap.ExternalKit;
+import com.iohao.game.bolt.broker.client.external.bootstrap.message.ExternalMessage;
+import com.xf.woz.protoBuf.ConnectVerify;
+import org.junit.Test;
+
+public class WebSocketClientTest {
+    @Test
+    public void decodeTest(){
+        int cmd = 0;
+        int subCmd = 0;
+        ConnectVerify connectReq = new ConnectVerify();
+
+        connectReq.setMsg("jack");
+        String s = JSONUtil.toJsonStr(connectReq);
+        ExternalMessage externalMessage = ExternalKit
+                // 路由、子路由、业务数据
+                .createExternalMessage(cmd, subCmd, connectReq);
+        System.out.println(externalMessage);
+//        System.out.println("CommentWebSocketClient.onOpen");
+        // 游戏框架内置的协议， 与游戏前端相互通信的协议
+    }
+}
+ */
+package com.xf.woz.client;
+
+import cn.hutool.json.JSONUtil;
+import com.iohao.game.bolt.broker.client.external.bootstrap.ExternalKit;
+import com.iohao.game.bolt.broker.client.external.bootstrap.message.ExternalMessage;
+import com.xf.woz.protoBuf.ConnectVerify;
+import org.junit.Test;
+
+public class WebSocketClientTest {
+    @Test
+    public void decodeTest(){
+        int cmd = 0;
+        int subCmd = 0;
+        ConnectVerify connectReq = new ConnectVerify();
+
+        connectReq.setMsg("jack");
+        String s = JSONUtil.toJsonStr(connectReq);
+        ExternalMessage externalMessage = ExternalKit
+                // 路由、子路由、业务数据
+                .createExternalMessage(cmd, subCmd, connectReq);
+        System.out.println(externalMessage);
+//        System.out.println("CommentWebSocketClient.onOpen");
+        // 游戏框架内置的协议， 与游戏前端相互通信的协议
+    }
+}
+ */
+package com.xf.woz.client;
+
+import cn.hutool.json.JSONUtil;
+import com.iohao.game.bolt.broker.client.external.bootstrap.ExternalKit;
+import com.iohao.game.bolt.broker.client.external.bootstrap.message.ExternalMessage;
+import com.xf.woz.protoBuf.ConnectVerify;
+import org.junit.Test;
+
+public class WebSocketClientTest {
+    @Test
+    public void decodeTest(){
+        int cmd = 0;
+        int subCmd = 0;
+        ConnectVerify connectReq = new ConnectVerify();
+
+        connectReq.setMsg("jack");
+        String s = JSONUtil.toJsonStr(connectReq);
+        ExternalMessage externalMessage = ExternalKit
+                // 路由、子路由、业务数据
+                .createExternalMessage(cmd, subCmd, connectReq);
+        System.out.println(externalMessage);
+//        System.out.println("CommentWebSocketClient.onOpen");
+        // 游戏框架内置的协议， 与游戏前端相互通信的协议
+    }
+}
